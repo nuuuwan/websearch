beb906a38f6741369c7f46598a2aba7c282305da
nuuuwan <nuuuwan@gmail.com>
2023-09-11 11:15:21 +0530

    Added google map and thesaurus

--- workflows/main.py
+++ workflows/main.py
@@ -1,6 +1,15 @@
 import sys
 import webbrowser
 
+from utils import Log
+
+log = Log('main')
+
+
+def open(url):
+    webbrowser.open(url)
+    log.info('Opening: ' + url)
+
 
 def book(search_term):
     for prefix in [
@@ -8,23 +17,38 @@ def book(search_term):
         'https://www.sarasavi.lk/serach-result?keyword=',
         'https://www.expo-graphic.com/search?keyword=',
         'https://jumpbooks.lk/?product_cat=&post_type=product&s=',
-        'https://cgabooksonline.com/search'+'?controller=search&orderby=position&orderway=desc&search_query=',
+        'https://cgabooksonline.com/search'
+        + '?controller=search&orderby=position&orderway=desc&search_query=',
         'https://bookyard.lk/?post_type=product&dgwt_wcas=1&s=',
         'https://samudrabooks.com/more_books_search?key=',
         'https://www.books.lk/?s=',
         'https://jeyabookcentre.com/search?key=',
         'https://www.pererahussein.com/books?search=',
-        'https://www.amazon.com/s?i=stripbooks-intl-ship&k='
-    ]: 
+        'https://www.amazon.com/s?i=stripbooks-intl-ship&k=',
+    ]:
         url = prefix + search_term
-        webbrowser.open(url)
-  
+        open(url)
+
+
+def thesaurus(search_term):
+    url = 'https://www.thesaurus.com/browse/' + search_term
+    open(url)
+
+
+def google_map(search_term):
+    url = 'https://www.google.com/maps/search/' + search_term
+    open(url)
+
 
 def main(search_type, search_term):
     if search_type == 'book':
         book(search_term)
+    elif search_type in ['thesaurus', 'the']:
+        thesaurus(search_term)
+    elif search_type in ['google_map', 'map', 'maps']:
+        google_map(search_term)
     else:
-        print('Invalid search type: ' + search_type)
+        log.error('Invalid search type: ' + search_type)
 
 
 if __name__ == '__main__':
